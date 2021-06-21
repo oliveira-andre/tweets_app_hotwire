742ced40b72b35f251c5466fd4bf7e6152e9850a
Andre Oliveira <andreoliveirar2d2@protonmail.com>
2021-06-21 01:14:16 -0300

    feat: adding enum and associating to user tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,16 @@
+# frozen_string_literal: true
+
 class Tweet < ApplicationRecord
   after_create_commit { broadcast_prepend_to :tweets }
   after_update_commit { broadcast_replace_to :tweets }
   after_destroy_commit { broadcast_remove_to :tweets }
 
-  validates_presence_of :body
+  enum status: {
+    privates: 0,
+    publics: 1
+  }
+
+  belongs_to :user
+
+  validates_presence_of :body, :status, :user_id
 end
